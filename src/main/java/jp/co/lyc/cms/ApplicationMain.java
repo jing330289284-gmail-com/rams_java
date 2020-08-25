57d7c81042fd60c63f3a6446cd7c002e4e8b8b69
yanghongfei <vinyoungfly@gmail.com>
2020-08-25 17:00:27 +0900

    添加了过滤器，可以删除每一个con中最上端的前端地址

--- src/main/java/jp/co/lyc/cms/ApplicationMain.java
+++ src/main/java/jp/co/lyc/cms/ApplicationMain.java
@@ -3,6 +3,9 @@ package jp.co.lyc.cms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,5 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApplicationMain {
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationMain.class, args);
+	}
+	@Bean
+	public FilterRegistrationBean registFilter(){
+		FilterRegistrationBean bean = new FilterRegistrationBean();
+		// 定义filter的过滤路径规则。
+		bean.addUrlPatterns("/*");
+		bean.setFilter(new Myfilter());
+        return bean;
 	}
 }
---
+++ src/main/java/jp/co/lyc/cms/Myfilter.java
@@ -0,0 +1,32 @@
+package jp.co.lyc.cms;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+ 
+public class Myfilter implements Filter{
+
+	    @Override
+	    public void init(FilterConfig filterConfig) throws ServletException {
+	 
+	    }
+	 
+	    @Override
+	    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+	        HttpServletRequest  request = (HttpServletRequest)servletRequest;
+	        HttpServletResponse response  = (HttpServletResponse) servletResponse;
+			response.setCharacterEncoding("UTF-8");
+		    response.setContentType("application/json; charset=utf-8");
+	        response.addHeader("Access-Control-Allow-Origin","http://127.0.0.1:3000");// * 允许所有域名跨域访问
+		    response.setHeader("Access-Control-Allow-Credentials","true");
+		    response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		    response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH");
+	        // 是否让请求通过过滤器。这行代码不能漏掉。如果漏掉请求会访问不到URL
+	        filterChain.doFilter(request,response);
+	    }
+	 
+	    @Override
+	    public void destroy() {
+	 
+	    }
+}
