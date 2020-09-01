52073d71fdc49aa8ce8d47efc821d0941f05113c
LYC <LYC@DESKTOP-SPU4BNM>
2020-09-01 13:37:39 +0900

    劉20200901

---
+++ src/main/java/jp/co/lyc/cms/util/StatusCodeToMsgMap.java
@@ -0,0 +1,31 @@
+package jp.co.lyc.cms.util;
+
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public abstract class StatusCodeToMsgMap {
+    private static Map<String, String> statusCodeToMsg = new HashMap<String, String>();
+
+    static {
+
+        statusCodeToMsg.put("MSG001", "[0]入力してください　");
+        statusCodeToMsg.put("MSG002", "ユーザー名またはパースワード入力が間違いました。");
+        statusCodeToMsg.put("MSG003", "[0]が入力間違いました");
+        statusCodeToMsg.put("MSG004", "データ存在してないです、検索条件を確認してください");
+        statusCodeToMsg.put("MSG005", "[0]を確認してください");
+        statusCodeToMsg.put("MSG006", "削除できない、[0]を確認してください");
+    }
+
+
+    public static String getErrMsgbyCode(String ecode) {
+        if (!StringUtils.hasText(ecode)) {
+            return "";
+        }
+
+        return statusCodeToMsg.get(ecode);
+    }
+
+}
