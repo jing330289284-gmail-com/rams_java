f470af67a6c3f03b60f6f3f4d9d46ed14c94beae
yanghongfei <vinyoungfly@gmail.com>
2020-09-08 18:59:53 +0900

    mytoastの実装

---
+++ src/main/java/jp/co/lyc/cms/validation/CustomerInfoValidation.java
@@ -0,0 +1,34 @@
+package jp.co.lyc.cms.validation;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import jp.co.lyc.cms.model.CustomerInfoModel;
+import jp.co.lyc.cms.util.StatusCodeToMsgMap;
+import jp.co.lyc.cms.util.UtilsCheckMethod;
+
+public class CustomerInfoValidation implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// TODO Auto-generated method stub
+		return CustomerInfoModel.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object obj, Errors errors) {
+		// TODO Auto-generated method stub
+		CustomerInfoModel p = (CustomerInfoModel) obj;
+
+		StackTraceElement elements[] = Thread.currentThread().getStackTrace();
+		for (int i = 0; i < elements.length; i++) {
+			StackTraceElement stackTraceElement = elements[i];
+			String methodName = stackTraceElement.getMethodName();
+			if (methodName.equals("toroku")) {
+				if (UtilsCheckMethod.isNullOrEmpty(p.getCustomerName())) {
+						errors.rejectValue("customerName", "", StatusCodeToMsgMap.getErrMsgbyCodeReplace("MSG001","社員名"));
+				}
+			}
+		}
+	}
+	
+}
