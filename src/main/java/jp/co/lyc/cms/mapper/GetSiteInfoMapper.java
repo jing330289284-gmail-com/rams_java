43dcd399aa9002d1895cf01f1dbd51d2a4fcabe8
jingda <jing330289284@gmail.com>
2020-09-21 20:21:07 +0900

    jingda20200921

--- src/main/java/jp/co/lyc/cms/mapper/GetSiteInfoMapper.java
+++ src/main/java/jp/co/lyc/cms/mapper/GetSiteInfoMapper.java
@@ -11,14 +11,27 @@ import jp.co.lyc.cms.model.SiteModel;
 @Mapper
 
 public interface GetSiteInfoMapper {
-
+	/**
+	 * 現場情報を登録
+	 * 
+	 */
 	public void siteInsert(Map<String, Object> sendMap);
-
-	public List<SiteModel> getSiteInfo(@Param("employeeNo")String employeeNo);
-	
+	/**
+	 * 現場情報を修正
+	 * 
+	 */
+	public void siteUpdate(Map<String, Object> sendMap);
 	/**
 	 * 現場情報を削除
 	 * 
 	 */
 	public void deleteEmployeeSiteInfo(Map<String, Object> sendMap);
+
+	/**
+	 * 現場情報を取得
+	 * 
+	 */
+	public List<SiteModel> getSiteInfo(@Param("employeeNo")String employeeNo);
+	
+
 }
