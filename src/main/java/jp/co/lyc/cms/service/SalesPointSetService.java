68e847a3185ecf43af26c53092f0c68354c4ebe5
jingda <jing330289284@gmail.com>
2020-10-05 18:48:42 +0900

    jingda20201005

--- src/main/java/jp/co/lyc/cms/service/SalesPointSetService.java
+++ src/main/java/jp/co/lyc/cms/service/SalesPointSetService.java
@@ -1,10 +1,12 @@
 package jp.co.lyc.cms.service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
 import jp.co.lyc.cms.mapper.SalesPointSetMapper;
@@ -26,8 +28,26 @@ public class SalesPointSetService {
 		}
 		return true;
 	}
-	
-	public boolean updateSiteInfo(Map<String, Object> sendMap) {
+
+	/**
+	 * 削除
+	 * 
+	 * @param sendMap
+	 */
+
+	public boolean salesPointDelete(HashMap<String, Object> sendMap) {
+		try {
+			salesPointSetMapper.salesPointDelete(sendMap);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	@Transactional(rollbackFor = Exception.class)
+	public boolean salesPointUpdate(Map<String, Object> sendMap) {
 		try {
 			salesPointSetMapper.salesPointUpdate(sendMap);
 		} catch (Exception e) {
