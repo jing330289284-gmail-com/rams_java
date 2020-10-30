343aaf48da2710b3cc73aa38f7f7606c5841ed22
LAPTOP-HRHTD6GD\eigyo <ty1913702383gmail.com>
2020-10-30 14:38:01 +0900

    20201030 tengyu

---
+++ src/main/java/jp/co/lyc/cms/mapper/SituationChangesMapper.java
@@ -0,0 +1,19 @@
+package jp.co.lyc.cms.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import jp.co.lyc.cms.model.SituationChangesModel;
+
+@Mapper
+public interface SituationChangesMapper {
+
+public List<SituationChangesModel>getSituationChanges(Map<String, Object> sendMap);
+
+public List<SituationChangesModel>getSituationChangesFront(Map<String, Object> sendMap);
+
+public List<SituationChangesModel>getSituationIntoORretirement(Map<String, Object> sendMap);
+
+}
