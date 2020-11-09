6ada13f73eb884f5e1e9590528839e2005f6b095
LUOXIAO <rassworking@gmail.com>
2020-11-09 15:38:24 +0900

    変更内容： １、営業個別売上（コントロール新規追加） ２、営業個別売上（サービス新規追加） ３、営業個別売上（モデル新規追加） ４、営業個別売上（マッパー新規追加） ５、営業個別売上（mybaitsファイル新規追加）

---
+++ src/main/java/jp/co/lyc/cms/model/SalesProfitModel.java
@@ -0,0 +1,122 @@
+package jp.co.lyc.cms.model;
+
+import java.util.Date;
+
+public class SalesProfitModel{
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -2028159323401651353L;
+	String rowNo;//番号
+	String yearAndMonth;//年月
+	String employeeStatus;//社員区分・返信同じ
+	String employeeFrom;//所属
+	String employeeName;//氏名
+	String customerName;//お客様・返信同じ
+	String workDate;//入出場期間
+	String unitPrice;//単価・返信同じ
+	String profit;//売上
+	String salary;//給料(発注額)
+	String siteRoleName;//粗利
+	//ＤＢ検索用↓返信用
+	String firstName;//姓
+	String lastName;//名
+	Date startDate;//現場開始時間
+	Date endDate;//現場終了時間
+
+	public String getRowNo() {
+		return rowNo;
+	}
+	public void setRowNo(String rowNo) {
+		this.rowNo = rowNo;
+	}
+	public String getYearAndMonth() {
+		return yearAndMonth;
+	}
+	public void setYearAndMonth(String yearAndMonth) {
+		this.yearAndMonth = yearAndMonth;
+	}
+	public String getEmployeeStatus() {
+		return employeeStatus;
+	}
+	public void setEmployeeStatus(String employeeStatus) {
+		this.employeeStatus = employeeStatus;
+	}
+	public String getEmployeeFrom() {
+		return employeeFrom;
+	}
+	public void setEmployeeFrom(String employeeFrom) {
+		this.employeeFrom = employeeFrom;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getWorkDate() {
+		return workDate;
+	}
+	public void setWorkDate(String workDate) {
+		this.workDate = workDate;
+	}
+	public String getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(String unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	public String getProfit() {
+		return profit;
+	}
+	public void setProfit(String profit) {
+		this.profit = profit;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	public String getSiteRoleName() {
+		return siteRoleName;
+	}
+	public void setSiteRoleName(String siteRoleName) {
+		this.siteRoleName = siteRoleName;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+}
