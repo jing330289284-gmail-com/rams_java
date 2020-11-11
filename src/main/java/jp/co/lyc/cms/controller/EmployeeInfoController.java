09b5785590e61a720e83c3030bca9a5c01787c71
劉林涛 <@DESKTOP-SPU4BNM>
2020-11-11 17:29:27 +0900

    劉林涛

--- src/main/java/jp/co/lyc/cms/controller/EmployeeInfoController.java
+++ src/main/java/jp/co/lyc/cms/controller/EmployeeInfoController.java
@@ -127,7 +127,6 @@ public class EmployeeInfoController extends BaseController {
 			sendMap = utilsController.upload(resumeInfo2, sendMap, "resumeInfo2", emp.getResumeName2());
 			sendMap = utilsController.upload(residentCardInfo, sendMap, "residentCardInfo", "在留カード");
 			sendMap = utilsController.upload(passportInfo, sendMap, "passportInfo", "パスポート");
-			//sendMap = utilsController.upload(pictures, sendMap, "picInfo", "写真");
 			employeeInfoService.insertEmployee((HashMap<String, Object>) sendMap);
 		} catch (Exception e) {
 			resultMap.put("result", false);
