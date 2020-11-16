f4ae53f571df8fe614afd49abf48f69d7f3fb8f7
wang <wangy@pjntpb-ja01.tecnos.jp>
2020-11-16 18:38:11 +0900

    営業個別売上更新

--- src/main/java/jp/co/lyc/cms/controller/SalesProfitController.java
+++ src/main/java/jp/co/lyc/cms/controller/SalesProfitController.java
@@ -1,5 +1,7 @@
 package jp.co.lyc.cms.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,10 +19,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import jp.co.lyc.cms.common.BaseController;
+import jp.co.lyc.cms.model.PersonalSalesSearchModel;
 import jp.co.lyc.cms.model.SalesInfoModel;
 import jp.co.lyc.cms.model.SalesPointSetModel;
 import jp.co.lyc.cms.model.SalesProfitModel;
+import jp.co.lyc.cms.service.PersonalSalesSearchService;
 import jp.co.lyc.cms.service.SalesProfitService;
+
+import org.joda.time.DateTime;
+import org.joda.time.Months;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 @Controller
 @CrossOrigin(origins = "http://127.0.0.1:3000")
@@ -30,6 +39,8 @@ public class SalesProfitController extends BaseController {
 
 	@Autowired
 	SalesProfitService salesProfitService;
+	@Autowired
+	PersonalSalesSearchService personalSalesSearchService;
 
 	String errorsMessage = "";
 
@@ -79,10 +90,113 @@ public class SalesProfitController extends BaseController {
 	@ResponseBody
 	public List<SalesInfoModel> getSalesInfo(@RequestBody SalesProfitModel salesProfitModel) {
 
-		// List<SalesProfitModel> siteList =
-		// salesProfitService.getSalesProfitInfo(salesProfitModel);
 		List<SalesInfoModel> siteList = salesProfitService.getSalesInfo(salesProfitModel);
 		logger.info("SalesProfitController.getSalesPointInfo:" + "検索結束");
+
+		for (int i = 0; i < siteList.size(); i++) {
+			String yearAndMonth = siteList.get(i).getAdmissionStartDate().substring(0, 6);
+			if (i > 0) {
+				if (yearAndMonth.equals(siteList.get(i - 1).getYearAndMonth())) {
+					yearAndMonth = "";
+				}
+			}
+			siteList.get(i).setYearAndMonth(yearAndMonth);
+			siteList.get(i).setCustomerName(
+					siteList.get(i).getCustomerAbbreviation() == null ? siteList.get(i).getCustomerName()
+							: siteList.get(i).getCustomerAbbreviation());
+			String employeeName = (siteList.get(i).getEmployeeFristName() == null ? ""
+					: siteList.get(i).getEmployeeFristName())
+					+ (siteList.get(i).getEmployeeLastName() == null ? "" : siteList.get(i).getEmployeeLastName());
+			siteList.get(i).setEmployeeName(employeeName);
+
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMM");
+			String startTime = dateFormat.format(salesProfitModel.getStartDate()).toString();
+			String endTime = dateFormat.format(salesProfitModel.getEndDate()).toString();
+			String admissionStartDate = siteList.get(i).getAdmissionStartDate().substring(0, 6);
+			String admissionEndDate = siteList.get(i).getAdmissionEndDate().substring(0, 6);
+			String workDateStart = admissionStartDate;
+			String workDateEnd = admissionEndDate;
+			if (Integer.parseInt(startTime) > Integer.parseInt(admissionStartDate)) {
+				workDateStart = startTime;
+			}
+
+			siteList.get(i).setWorkDate(workDateStart + " ~ " + workDateEnd);
+
+			DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM");
+			int months = Months
+					.monthsBetween(
+							formatter
+									.parseDateTime(workDateStart.substring(0, 4) + "-" + workDateStart.substring(4, 6)),
+							formatter.parseDateTime(workDateEnd.substring(0, 4) + "-" + workDateEnd.substring(4, 6)))
+					.getMonths() + 1;
+			siteList.get(i).setProfit(Integer.toString(Integer.parseInt(siteList.get(i).getUnitPrice()) * months));
+
+			String startYandM = workDateStart;
+			String endYandM = workDateEnd;
+			List<String> getYandM = new ArrayList<String>();
+			if (startYandM != "0" && startYandM != null && endYandM != "0" && endYandM != null) {
+				int startY = Integer.parseInt(startYandM.substring(0, 4));
+				int startM = Integer.parseInt(startYandM.substring(4, 6));
+				int endY = Integer.parseInt(endYandM.substring(0, 4));
+				int endM = Integer.parseInt(endYandM.substring(4, 6));
+				int count = 0;
+				for (int y = startY; y <= endY; y++) {
+					if (y == startY && y == endY) {
+						for (int m = startM; m <= endM; m++) {
+							String monthStr = Integer.toString(m);
+							if (m < 10) {
+								monthStr = "0" + Integer.toString(m);
+							}
+							getYandM.add(count, Integer.toString(startY) + monthStr);
+							count++;
+						}
+					} else if (y == startY) {
+						for (int m = startM; m <= 12; m++) {
+
+							String monthStr = Integer.toString(m);
+							if (m < 10) {
+								monthStr = "0" + Integer.toString(m);
+							}
+							getYandM.add(count, Integer.toString(startY) + monthStr);
+							count++;
+						}
+					} else if (y != startY && y != endY) {
+						for (int m = 1; m <= 12; m++) {
+
+							String monthStr = Integer.toString(m);
+							if (m < 10) {
+								monthStr = "0" + Integer.toString(m);
+							}
+							getYandM.add(count, Integer.toString(y) + monthStr);
+							count++;
+						}
+					} else if (y == endY) {
+						for (int m = 1; m <= endM; m++) {
+
+							String monthStr = Integer.toString(m);
+							if (m < 10) {
+								monthStr = "0" + Integer.toString(m);
+							}
+							getYandM.add(count, Integer.toString(endY) + monthStr);
+							count++;
+						}
+					}
+
+				}
+			}
+			Map<String, Object> sendMap = new HashMap<String, Object>();
+			sendMap.put("getYandM", getYandM);
+			sendMap.put("employeeName", siteList.get(i).getEmployeeNo());
+			List<PersonalSalesSearchModel> personModelList = new ArrayList<PersonalSalesSearchModel>();
+			personModelList = personalSalesSearchService.searchEmpDetails(sendMap);
+			int salary = 0;
+			for (int j = 0; j < personModelList.size(); j++) {
+				salary += Integer.parseInt(personModelList.get(j).getSalary());
+			}
+			siteList.get(i).setSalary(Integer.toString(salary));
+			siteList.get(i).setRowNo(Integer.toString(i + 1));
+			siteList.get(i).setSiteRoleName(Integer.toString(salary - Integer.parseInt(siteList.get(i).getProfit())));
+		}
 		return siteList;
 	}
 
@@ -91,48 +205,32 @@ public class SalesProfitController extends BaseController {
 	 *
 	 * @return
 	 */
-	public Map<String, Object> putData(SalesPointSetModel salesPointSetModel) {
-		HttpSession loginSession = getSession();
-		Map<String, Object> sendMap = new HashMap<String, Object>();
-		String no = salesPointSetModel.getNo();
-		String employee = salesPointSetModel.getEmployee();
-		String newMember = salesPointSetModel.getNewMember();
-		String customerContract = salesPointSetModel.getCustomerContract();
-		String level = salesPointSetModel.getLevel();
-		String salesPuttern = salesPointSetModel.getSalesPuttern();
-		String specialPoint = salesPointSetModel.getSpecialPoint();
-		String point = salesPointSetModel.getPoint();
-		String remark = salesPointSetModel.getRemark();
-
-		if (no != null && no.length() != 0) {
-			sendMap.put("no", no);
-		}
-		if (employee != null && employee.length() != 0) {
-			sendMap.put("employee", employee);
-		}
-		if (newMember != null && newMember.length() != 0) {
-			sendMap.put("newMember", newMember);
-		}
-		if (customerContract != null && customerContract.length() != 0) {
-			sendMap.put("customerContract", customerContract);
-		}
-		if (level != null && level.length() != 0) {
-			sendMap.put("level", level);
-		}
-		if (salesPuttern != null && salesPuttern.length() != 0) {
-			sendMap.put("salesPuttern", salesPuttern);
-		}
-		if (specialPoint != null && specialPoint.length() != 0) {
-			sendMap.put("specialPoint", specialPoint);
-		}
-		if (point != null && point.length() != 0) {
-			sendMap.put("point", point);
-		}
-		if (remark != null && remark.length() != 0) {
-			sendMap.put("remark", remark);
-		}
-
-		sendMap.put("updateUser", loginSession.getAttribute("employeeName"));
-		return sendMap;
-	}
+	/*
+	 * public Map<String, Object> putData(SalesPointSetModel salesPointSetModel) {
+	 * HttpSession loginSession = getSession(); Map<String, Object> sendMap = new
+	 * HashMap<String, Object>(); String no = salesPointSetModel.getNo(); String
+	 * employee = salesPointSetModel.getEmployee(); String newMember =
+	 * salesPointSetModel.getNewMember(); String customerContract =
+	 * salesPointSetModel.getCustomerContract(); String level =
+	 * salesPointSetModel.getLevel(); String salesPuttern =
+	 * salesPointSetModel.getSalesPuttern(); String specialPoint =
+	 * salesPointSetModel.getSpecialPoint(); String point =
+	 * salesPointSetModel.getPoint(); String remark =
+	 * salesPointSetModel.getRemark();
+	 * 
+	 * if (no != null && no.length() != 0) { sendMap.put("no", no); } if (employee
+	 * != null && employee.length() != 0) { sendMap.put("employee", employee); } if
+	 * (newMember != null && newMember.length() != 0) { sendMap.put("newMember",
+	 * newMember); } if (customerContract != null && customerContract.length() != 0)
+	 * { sendMap.put("customerContract", customerContract); } if (level != null &&
+	 * level.length() != 0) { sendMap.put("level", level); } if (salesPuttern !=
+	 * null && salesPuttern.length() != 0) { sendMap.put("salesPuttern",
+	 * salesPuttern); } if (specialPoint != null && specialPoint.length() != 0) {
+	 * sendMap.put("specialPoint", specialPoint); } if (point != null &&
+	 * point.length() != 0) { sendMap.put("point", point); } if (remark != null &&
+	 * remark.length() != 0) { sendMap.put("remark", remark); }
+	 * 
+	 * sendMap.put("updateUser", loginSession.getAttribute("employeeName")); return
+	 * sendMap; }
+	 */
 }
--- src/main/java/jp/co/lyc/cms/model/SalesInfoModel.java
+++ src/main/java/jp/co/lyc/cms/model/SalesInfoModel.java
@@ -1,18 +1,46 @@
 package jp.co.lyc.cms.model;
+
 public class SalesInfoModel {
 
 	/**
 	 *
 	 */
 	private static final long serialVersionUID = -2028159323401651353L;
-	String employeeFristName;	//	氏名
-	String employeeLastName;	//	苗字
-	String customerName;	//	会社名
-	String customerAbbreviation;	//	会社略称
-	String salary;	//	給料
-	String unitPrice;	//	単価
-	String admissionStartDate;	//	現場開始年月
-	String admissionEndDate;	//	現場終了年月
+	String employeeFristName; // 氏名
+	String employeeLastName; // 苗字
+	String customerName; // 会社名
+	String customerAbbreviation; // 会社略称
+	String salary; // 給料
+	String unitPrice; // 単価
+	String admissionStartDate; // 現場開始年月
+	String admissionEndDate; // 現場終了年月
+	String employeeStatus; // 社員区分
+	String employeeFrom; // 所属
+	// データ処理
+	String rowNo; // 自動採番
+	String employeeNo; // 社員番号
+	String startEndDate; // 現場開始から終了年月
+	String employeeName; // 氏名
+	String yearAndMonth; // 年月
+	String workDate; // 入場期間
+	String profit; // 売上
+	String siteRoleName; // 粗利
+
+	public String getRowNo() {
+		return rowNo;
+	}
+
+	public void setRowNo(String rowNo) {
+		this.rowNo = rowNo;
+	}
+
+	public String getEmployeeNo() {
+		return employeeNo;
+	}
+
+	public void setEmployeeNo(String employeeNo) {
+		this.employeeNo = employeeNo;
+	}
 
 	public String getAdmissionStartDate() {
 		return admissionStartDate;
@@ -76,6 +104,70 @@ public class SalesInfoModel {
 
 	public void setUnitPrice(String unitPrice) {
 		this.unitPrice = unitPrice;
+	}
+
+	public String getStartEndDate() {
+		return startEndDate;
+	}
+
+	public void setStartEndDate(String startEndDate) {
+		this.startEndDate = startEndDate;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public String getYearAndMonth() {
+		return yearAndMonth;
+	}
+
+	public void setYearAndMonth(String yearAndMonth) {
+		this.yearAndMonth = yearAndMonth;
+	}
+
+	public String getEmployeeStatus() {
+		return employeeStatus;
+	}
+
+	public void setEmployeeStatus(String employeeStatus) {
+		this.employeeStatus = employeeStatus;
+	}
+
+	public String getEmployeeFrom() {
+		return employeeFrom;
+	}
+
+	public void setEmployeeFrom(String employeeFrom) {
+		this.employeeFrom = employeeFrom;
+	}
+
+	public String getWorkDate() {
+		return workDate;
+	}
+
+	public void setWorkDate(String workDate) {
+		this.workDate = workDate;
+	}
+
+	public String getProfit() {
+		return profit;
+	}
+
+	public void setProfit(String profit) {
+		this.profit = profit;
+	}
+
+	public String getSiteRoleName() {
+		return siteRoleName;
+	}
+
+	public void setSiteRoleName(String siteRoleName) {
+		this.siteRoleName = siteRoleName;
 	}
 
 	public static long getSerialversionuid() {
